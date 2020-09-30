f61d904418371e430b3415eab36a3f06e30a6662
David Allen <davidallen02@me.com>
2020-09-30 10:35:16 -0700

    Add starts

---
+++ R/inventory-months-of-supply.R
@@ -0,0 +1,14 @@
+library(magrittr)
+
+dat <- pamngr::get_data("ehslmssa") %>%
+  tidyr::pivot_longer(cols = -dates, names_to = "variable")
+
+p <- dat %>%
+  pamngr::lineplot() %>%
+  pamngr::pam_plot(
+    plot_title = "Existing Homes for Sale",
+    plot_subtitle = "Millions",
+    show_legend = FALSE
+  )
+
+p %>% pamngr::all_output("existing-homes-inventory-months-of-supply")
